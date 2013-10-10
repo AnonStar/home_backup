fb9cff39d171f48e14072cd8364da24323eb47d1
basti <Sebastian Gumprich>
2013-10-10 18:43:12 +0200

    Replaced "rsyncbackup" with "sh".

--- home_backup/home_backup.py
+++ home_backup/home_backup.py
@@ -2,41 +2,39 @@
 
 import os
 import shutil
-import logging
-import RSyncBackup
 import time
 
-logging.basicConfig()
+from sh import rsync
+
 
 # Functions
 
 
 def check_dir_exist(os_dir):
-	if not os.path.exists(os_dir):
-		print os_dir, "does not exist."
-		exit(1)
+    if not os.path.exists(os_dir):
+        print os_dir, "does not exist."
+        exit(1)
 
 
 def confirm():
-	gogo = raw_input("Continue? yes/no\n")
-	global exit_condition
-	if gogo == 'yes':
-		exit_condition = 0
-		return exit_condition
-	elif gogo == "no":
-		exit_condition = 1
-		return exit_condition
-	else:
-		print "Please answer with yes or no."
-		confirm()
+    gogo = raw_input("Continue? yes/no\n")
+    global exit_condition
+    if gogo == 'yes':
+        exit_condition = 0
+        return exit_condition
+    elif gogo == "no":
+        exit_condition = 1
+        return exit_condition
+    else:
+        print "Please answer with yes or no."
+        confirm()
 
 
 def delete_files(ending):
-	os.chdir(backup_path)
-	for r, d, f in os.walk(backup_path):
-		for files in f:
-			if files.endswith("." + ending):
-				os.remove(os.path.join(r, files))
+    for r, d, f in os.walk(backup_path):
+        for files in f:
+            if files.endswith("." + ending):
+                os.remove(os.path.join(r, files))
 
 
 # Specify what and where to backup.
@@ -45,7 +43,7 @@ check_dir_exist(backup_path)
 print "Okay", backup_path, "will be saved."
 time.sleep(3)
 
-backup_to_path = raw_input("Where to backup?")
+backup_to_path = raw_input("Where to backup?\n")
 check_dir_exist(backup_to_path)
 
 
@@ -53,30 +51,26 @@ check_dir_exist(backup_to_path)
 print "First, let's cleanup unnecessary files in the backup path."
 file_types = ["tmp", "bak", "dmp"]
 for file_type in file_types:
-	print "Delete", file_type, "files?"
-	confirm()
-	if exit_condition == 0:
-		delete_files(file_type)
+    print "Delete", file_type, "files?"
+    confirm()
+    if exit_condition == 0:
+        delete_files(file_type)
 
 
 # Empty trash can
 print "Empty trash can?"
 confirm()
 if exit_condition == 0:
-	print "emptying"
-	shutil.rmtree("~/.local/share/Trash/files")
+    print "Emptying!"
+    shutil.rmtree(os.path.expanduser("~/.local/share/Trash/files"))
 
 
 # Do the actual backup
 print "Doing the backup now!"
 confirm()
 if exit_condition == 1:
-		print "Aborting!"
-		exit(1)
+        print "Aborting!"
+        exit(1)
 
-
-backup = RSyncBackup.RSyncBackup(rsync="/usr/bin/rsync")
-
-backup.backup(source = backup_path,
-	destination = backup_to_path+"/",
-	excludeList = ['lost+found', '/sys', '/tmp', '/proc', '/mnt', '/dev', '/backup'])
+rsync("-auhv", "--delete", "--exclude=lost+found", "--exclude=/sys", "--exclude=/tmp", "--exclude=/proc",
+      "--exclude=/mnt", "--exclude=/dev", "--exclude=/backup", backup_path, backup_to_path)
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(
-	name='home_backup',
-	version='1.0',
-	packages=['home_backup'],
-	url='http://zufallsheld.de/2013/09/29/python-backup-script-with-rsync/',
-	license='GPL 3.0',
-	author='Sebastian Gumprich',
-	author_email='sebastian.gumprich@38.de',
-	description='simple python backup script',
-    long_description=open('README.txt').read()
+    name='home_backup',
+    version='1.0',
+    packages=['home_backup'],
+    url='http://zufallsheld.de/2013/09/29/python-backup-script-with-rsync/',
+    license='GPL 3.0',
+    author='Sebastian Gumprich',
+    author_email='sebastian.gumprich@38.de',
+    description='simple python backup script',
+    long_description=open('README.md').read()
 )
